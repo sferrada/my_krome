5454ffe8608f1d30e81a00c58c747f8a15419e57
Simon Ferrada Chamorro <simonfch72@gmail.com>
2024-03-12 23:15:20 -0300

    Backup

--- tests/cloudSurface/test.py
+++ tests/cloudSurface/test.py
@@ -1,11 +1,11 @@
 """
     The following script provides a benchmark for
     gas-grain chemistry with KROME using the PyKROME
-    Python interface. The script is based on the
-    framework provided by Semenov et al. 2010 and is
-    adapted from the original test.f90 file provided
-    with the KROME source code.
-    Initial conditions from Semenov et al. 2010 as well.
+    Python interface. It is based on the original
+    test.f90 file provided with the KROME source code.
+
+    Initial conditions and species abundances have been
+    adopted from Semenov et al. 2010.
 """
 import numpy as np
 import ctypes as ctypes
@@ -34,34 +34,34 @@ if __name__ == "__main__":
 
     # Set initial abundances
     x_min = 1e-40
-    x_mol = np.ones(pyk.krome_nmols) * x_min
-    x_mol[pyk.krome_idx_H2]  = 0.50e+00 * x_H
-    x_mol[pyk.krome_idx_HE]  = 9.00e-02 * x_H
-    x_mol[pyk.krome_idx_Cj]  = 1.20e-04 * x_H
-    x_mol[pyk.krome_idx_N]   = 7.60e-05 * x_H
-    x_mol[pyk.krome_idx_O]   = 2.56e-04 * x_H
-    x_mol[pyk.krome_idx_Sj]  = 8.00e-08 * x_H
-    x_mol[pyk.krome_idx_SIj] = 8.00e-09 * x_H
-    x_mol[pyk.krome_idx_FEj] = 3.00e-09 * x_H
-    x_mol[pyk.krome_idx_NAj] = 2.00e-09 * x_H
-    x_mol[pyk.krome_idx_MGj] = 7.00e-09 * x_H
-    x_mol[pyk.krome_idx_CLj] = 1.00e-09 * x_H
-    x_mol[pyk.krome_idx_Pj]  = 2.00e-10 * x_H
+    abund = np.ones(pyk.krome_nmols) * x_min
+    abund[pyk.krome_idx_H2]  = 0.50e+00 * x_H
+    abund[pyk.krome_idx_HE]  = 9.00e-02 * x_H
+    abund[pyk.krome_idx_Cj]  = 1.20e-04 * x_H
+    abund[pyk.krome_idx_N]   = 7.60e-05 * x_H
+    abund[pyk.krome_idx_O]   = 2.56e-04 * x_H
+    abund[pyk.krome_idx_Sj]  = 8.00e-08 * x_H
+    abund[pyk.krome_idx_SIj] = 8.00e-09 * x_H
+    abund[pyk.krome_idx_FEj] = 3.00e-09 * x_H
+    abund[pyk.krome_idx_NAj] = 2.00e-09 * x_H
+    abund[pyk.krome_idx_MGj] = 7.00e-09 * x_H
+    abund[pyk.krome_idx_CLj] = 1.00e-09 * x_H
+    abund[pyk.krome_idx_Pj]  = 2.00e-10 * x_H
 
     # Compute electrons abundance (neutral cloud)
-    x_mol[pyk.krome_idx_E] = pyk.lib.krome_get_electrons(x_mol[:])
+    abund[pyk.krome_idx_E] = pyk.lib.krome_get_electrons(abund[:])
 
     # Get and set dust initial abundance
     mmw = 1.43  # mean molecular weight
+    dust_rho0 = 3.0  # material density, g cm-3
     dust_to_gas = 1e-2  # dust-to-gas mass ratio
-    dust_mat_density = 3.0  # g cm-3
 
-    rho_gas = pyk.lib.krome_get_rho(x_mol[:])
+    rho_gas = pyk.lib.krome_get_rho(abund[:])
     dust_mass = 4. / 3. * np.pi * adust3.value
-    dust_abun = dust_to_gas * rho_gas / dust_mass / mmw
+    dust_abund = dust_to_gas * rho_gas / dust_mass / mmw
 
-    x_mol[pyk.krome_idx_GRAIN0] = dust_abun
-    pyk.lib.krome_set_user_xdust(dust_abun)
+    abund[pyk.krome_idx_GRAIN0] = dust_abund
+    pyk.lib.krome_set_user_xdust(dust_abund)
 
     # Set initial time and time step
     d_time = ctypes.c_double(1.0 * spy)
@@ -75,7 +75,7 @@ if __name__ == "__main__":
             print(f"n_step = {n_step:3d}; t_curr = {t_curr.value / spy:.2e} yr")
 
         # Call KROME
-        pyk.lib.krome(x_mol[:], ctypes.byref(Tgas), ctypes.byref(d_time))
+        pyk.lib.krome(abund[:], ctypes.byref(Tgas), ctypes.byref(d_time))
 
         # Update time
         t_curr = ctypes.c_double(t_curr.value + d_time.value)
@@ -85,9 +85,9 @@ if __name__ == "__main__":
         # Append output
         out_curr = np.insert(
             np.where(
-                x_mol < x_min,
+                abund < x_min,
                 x_min,
-                x_mol / x_H
+                abund / x_H
             ),
             0,
             t_curr.value / spy
